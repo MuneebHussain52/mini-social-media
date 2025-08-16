9f6ea2766c4440b5e72616548dc3d6df5f0804c1
Muneeb Hussain <146110760+MuneebHussain52@users.noreply.github.com>
2025-08-16 12:45:22 +0500

    Delete Mini-social-media directory

--- Mini-social-media/Backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { Login, Signup, Logout } = require("../controller/auth/auth");
-const router = express.Router();
-
-router.post("/login", Login);
-router.post("/signup", Signup);
-router.post("/logout", Logout);
-
-module.exports = router;
